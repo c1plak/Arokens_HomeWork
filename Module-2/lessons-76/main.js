7f2f281a3adfd8eadbae35ea6e45267cb538fbe4
c1plak <93764925+c1plak@users.noreply.github.com>
2023-11-28 19:19:04 +0700

    Delete Module-2/lessons-76 directory

--- Module-2/lessons-76/main.js
+++
@@ -1,7 +0,0 @@
-let firstName = 'Kirill'; // let - директива, которая используется для объявления переменной с возможностью переприсвоить значение этой переменной
-let surname = 'Fyodorov';
-const patronymic = 'Alexandrovich'; // const - директива, которая используется для объявления констант, что не позволит изменить значение таких переменных в будущем
-let age = 23;
-let favDish = 'spaghetti';
-
-console.log(surname, firstName, patronymic, age, 'y.o.', 'Favorite dish is', favDish);
